03bb5d42a9f14baf7b85fd7ce4ff5dfc7e886f20
harshini1510 <harshinihashini1510@gmail.com>
2025-06-06 15:01:29 +0530

    first commit

---
+++ harshu.js
@@ -0,0 +1,9 @@
+function sub(a,b){
+    return a-b;
+}
+console.log(sub(4,5));
+console.log(sub(2,5));
+
+const choices=['rock','paper','scissor']
+let computer=choices  [Math.floor(Math.random()*3)]
+console.log(computer);
